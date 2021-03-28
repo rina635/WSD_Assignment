7fcdc76777ff3237d31cd032fa3d0f027d89f098
ashhingle <ash.hingle@gmail.com>
2021-03-28 13:44:57 -0400

    files after our meeting on 3/28

---
+++ decision_list/decision-list.py
@@ -0,0 +1,183 @@
+'''
+AIT 590 - Assignment 3 
+
+Team 3 - Rafeef Baamer, Ashish Hingle, Rina Lidder, & Andy Nguyen
+
+
+
+
+'''
+
+
+
+import nltk, string, re, math, sys
+from nltk.probability import ConditionalFreqDist
+from nltk.probability import ConditionalProbDist
+from nltk.probability import LidstoneProbDist
+from nltk.corpus import stopwords
+from bs4 import BeautifulSoup
+
+
+# command line arguments for the file sources of training data, testing data, decision list
+training_data = sys.argv[1]
+testing_data = sys.argv[2]
+my_decision_list = sys.argv[3]
+
+# Ambiguous word
+root = "line"
+
+# initializing the decision list to an empty list
+decision_list = []
+
+
+# Function to preprocess the textual content
+def process_text(text):
+    text = text.lower()
+
+    # removing the standard stop word from the text
+    stop_words = stopwords.words("english")
+    stop_words.extend(string.punctuation)
+    
+    # treating "lines" and "line" as a single entity
+    text = text.replace("lines", "line")
+    corpus = [re.sub(r'[\.\,\?\!\'\"\-\_/]','',w) for w in text.split(" ")]
+    corpus = [w for w in corpus if w not in stop_words and w != '']
+    return corpus
+
+
+# Function to retrieve a word at a certain index
+def get_n_word(n, context):
+    root_index = context.index(root)
+    n_word_index = root_index + n
+    if len(context) > n_word_index and n_word_index >= 0:
+        return context[n_word_index]
+    else:
+        return ""
+
+# Function to add a new "condition" learned from the training data to the decision list
+def add_word_cond(cfd, data, n):
+    for element in data:
+        sense, context = element['sense'], element['text']
+        n_word = get_n_word(n, context)
+        if n_word != '':
+            condition = str(n) + "_word_" + re.sub(r'\_', '', n_word)
+            cfd[condition][sense] += 1
+    return cfd
+
+
+# Calculate the logarithm of ratio of sense probabilities
+def calculate_log_likelihood(cpd, rule):
+    prob = cpd[rule].prob("phone")
+    prob_star = cpd[rule].prob("product")
+    div = prob / prob_star
+    if div == 0:
+        return 0
+    else:
+        return math.log(div, 2)
+
+
+# checking whether the rule is satisfied in a given context
+def check_rule(context, rule):
+    rule_scope, rule_type, rule_feature = rule.split("_")
+    rule_scope = int(rule_scope)
+    
+    return get_n_word(rule_scope, context) == rule_feature
+        
+# Function to predict the sense on test data
+def predict(context, majority_label):
+    for rule in decision_list:
+        if check_rule(context, rule[0]):
+            if rule[1] > 0:
+                return ("phone", context, rule[0])
+            elif rule[1] < 0:
+                return ("product", context, rule[0])
+    return (majority_label, context, "default")
+
+
+# Extracting the textual content from training data through XML parsing
+with open(training_data, 'r') as data:
+    soup = BeautifulSoup(data, 'html.parser')
+train_data = []
+for instance in soup.find_all('instance'):
+    sntnc = dict()
+    sntnc['id'] = instance['id']
+    sntnc['sense'] = instance.answer['senseid']
+    text = ""
+    for s in instance.find_all('s'):
+        text = text+ " "+ s.get_text()
+    sntnc['text'] = process_text(text)
+    train_data.append(sntnc)
+
+# Use conditional frequency distribution to add learned rules to the decision list
+cfd = ConditionalFreqDist()
+cfd = add_word_cond(cfd, train_data, 1)
+cfd = add_word_cond(cfd, train_data, -1)
+cfd = add_word_cond(cfd, train_data, 2)
+cfd = add_word_cond(cfd, train_data, -2)
+#cfd = add_word_cond(cfd, train_data, 3)
+#cfd = add_word_cond(cfd, train_data, -3)
+#cfd = add_word_cond(cfd, train_data, 4)
+#cfd = add_word_cond(cfd, train_data, -4)
+#cfd = add_word_cond(cfd, train_data, 5)
+#cfd = add_word_cond(cfd, train_data, -5)
+#cfd = add_word_cond(cfd, train_data, 6)
+#cfd = add_word_cond(cfd, train_data, -6)
+#cfd = add_word_cond(cfd, train_data, 7)
+#cfd = add_word_cond(cfd, train_data, -7)
+#cfd = add_word_cond(cfd, train_data, 8)
+#cfd = add_word_cond(cfd, train_data, -8)
+
+
+# Instantiating Condition probability distribution to calculate the probabilities of the frequencies recorded above
+cpd = ConditionalProbDist(cfd, LidstoneProbDist, 0.1)
+
+# storing the learned rules into the decision list
+for rule in cpd.conditions():
+
+    likelihood = calculate_log_likelihood(cpd, rule)
+    decision_list.append([rule, likelihood, "phone" if likelihood > 0 else "product"])
+    
+    decision_list.sort(key=lambda rule: math.fabs(rule[1]), reverse=True)
+
+
+# extracting the test data through XML parsing
+with open(testing_data, 'r') as data:
+    test_soup = BeautifulSoup(data, 'html.parser')
+
+test_data = []
+for instance in test_soup('instance'):
+    sntnc = dict()
+    sntnc['id'] = instance['id']
+    text = ""
+    for s in instance.find_all('s'):
+        text = text+ " "+ s.get_text()
+    sntnc['text'] = process_text(text)
+    test_data.append(sntnc)
+
+# Calculating the frequencies of each senses
+senseA, senseB = 0.0, 0.0
+for element in train_data:
+    if element['sense'] == "phone":
+        senseA += 1.0
+    elif element['sense'] == 'product':
+        senseB += 1.0
+    else:
+        print("warning no match")
+
+# Calculating the majority sense
+majority_sense = "phone" if senseA > senseB else "product"
+
+# Performing the predictions
+predictions = []
+for element in test_data:
+    pred, _, r = predict(element['text'], majority_sense)
+    id1 = element['id']
+    predictions.append(f'<answer instance="{id1}" senseid="{pred}"/>')
+    print(f'<answer instance="{id1}" senseid="{pred}"/>')
+
+
+# Storing the decision list into a file
+with open(my_decision_list, 'w') as output:  
+    for listitem in decision_list:
+        output.write('%s\n' % listitem)
+
---
+++ decision_list/scorer.py
@@ -0,0 +1,87 @@
+'''
+AIT 590 - Assignment 3 
+
+Team 3 - Rafeef Baamer, Ashish Hingle, Rina Lidder, & Andy Nguyen
+
+This is the Scorer
+
+
+'''
+
+
+
+
+
+import pandas as pd
+import re
+import sys
+
+
+# command line arguments for the file sources of results obtained from decision-list and available gold standard
+my_key = sys.argv[1]
+gs_key = sys.argv[2]
+
+#function to search the strings and get keys, sense
+def get_senses(mylist):
+    sense = {}
+    keys = []
+    for string in mylist:
+        search = re.search('<answer instance="(.*)" senseid="(.*)"/>', string, re.IGNORECASE)
+        key = search.group(1)
+        keys.append(key)
+        value = search.group(2)
+        sense[key] = value
+    return sense, keys
+
+
+
+#open the two files taken as input from command line and strip out '\n'
+with open(gs_key, 'r') as data:
+    mylist1 = [line.rstrip('\n') for line in data]
+answers, keys = get_senses(mylist1)
+
+with open(my_key, 'r') as data:
+    mylist2 = [line.rstrip('\n') for line in data]
+preds, keys = get_senses(mylist2)
+
+
+# For a specific key check if the values matches
+correct = 0
+total = len(keys)
+for key in keys:
+    if(answers[key] == preds[key]):
+        correct += 1
+correct
+
+#caluclated the most frequent sense baseline
+baseline_count = 0
+for key in keys:
+    if(answers[key] == 'phone'):
+        baseline_count += 1
+baseline_acc = (float(baseline_count)/float(total))*100
+print("Baseline accuracy is "+str(baseline_acc)+"%")
+
+
+# calucalate the accuracy after learning features
+accuracy = (float(correct)/float(total))*100
+print("Accuracy after adding learned features is "+str(accuracy)+"%")
+
+
+#creating array for our output and append the values to list
+pred_list = []
+for v in preds:
+    pred_list.append(preds[v])
+    
+
+#creating array for gold standard and append the values to its list
+answers_list = []
+for v in answers:
+    answers_list.append(answers[v])
+
+# creating dataframes for both the files
+df1 = pd.Series( (v for v in pred_list) )
+df2 = pd.Series( (v for v in answers_list) )
+
+#generating confusion matrix
+df_confusion = pd.crosstab(df1, df2)
+print("Confusion matrix is\n"  +str(df_confusion))
