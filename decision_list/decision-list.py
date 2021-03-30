3adc75449597b374c5002392db85984c3c493c0a
ashhingle <ash.hingle@gmail.com>
2021-03-30 13:38:11 -0400

    meeting 3/30

--- decision_list/decision-list.py
+++ decision_list/decision-list.py
@@ -2,18 +2,52 @@
 AIT 590 - Assignment 3 
 Team 3 - Rafeef Baamer, Ashish Hingle, Rina Lidder, & Andy Nguyen
 Date: 3/31/2021
-Description:
-Libraries used: 
 
-Additional features: 
+Description: This Python program implements a decision list classifier to perform word sense disambiguation. The methodology used is based on Yarowsky's method for WSD
+(Paper titled "DECISION LISTS FOR LEXICAL AMBIGUITY RESOLUTION: Application to Accent Restoration in Spanish and French). The program uses 6 features from a training set
+of data ranging from -3 to +3, based on the paper. The program uses a training file with answers to instruct the sense of an ambiguous word.
 
-Usage Instructions:
+Libraries used: NLTK, RE, MATH, SYS, BS4
 
+Usage Instructions: This program must be used in combination with the scorer.py program. To use the program:
+1) Ensure all supplmentary files are placed in the correct directory
+2) In the terminal, run the command "python decision-list.py line-train.xml line-test.xml my-decision-list.txt > my-line-answers.txt"
+	a) decision-list.py -> this file
+	b) line-train.xml -> file that contains training data with answers for each sense of the ambiguous word
+	c) line-test.xml -> file that contains test data (no answers present)
+	d) my-decision-list.xml -> a log file used during debugging
+	e) my-line-answers.txt -> file containing generated answers and sense for test data based on the training data
+ 
 Algorithm defined in program:
+Step 1: Extract the training data from XML files (preprocesses stopwords, punctuation, case, and symbols during extraction).
+Step 2: Store conditional probability distributions of training data in decision list.
+Step 3: Search the training data to count the frequency of each sense.
+Step 4: Calculate the majority sense based frequency likelihood for rules
+Step 5: Extract the testing data from XML files (preprocesses stopwords, punctuation, case, and symbols during extraction).
+Step 6: Use majority sense to predict the test data
+Step 7: Store decision list into my-line-answers.txt file
+
+Additional features:  
+
+
+Results of confusion Matrix:
+Baseline accuracy: 57.14%
+Based on the collocative features extracted the accuracy is 80.16%
+Confusion matrix is
+col_0    phone  product
+row_0
+phone       53        6
+product     19       48
+
+First 10 results of my-decision-list:
+
+
+First 10 results my-line-answers.txt:
+
+
 
 
 '''
-
 
 #Import libraries
 import nltk, string, re, math, sys
@@ -27,8 +61,6 @@ import time
 
 
 # command line arguments for the file sources of training data, testing data, decision list
-print(sys.argv[1])
-print(sys.argv[2])
 training_data = sys.argv[1]
 testing_data = sys.argv[2]
 my_decision_list = sys.argv[3]
@@ -64,27 +96,13 @@ def process_text(unprocessed_text):
     
     return processed_text   # return the processed text to the method that called it
 
-#this function finds the index of the collocative word
-def find_index(context, word):
-    word_index = []
-    i = 0
-    while True:
-        try: 
-            i = context.index(word, i) + 1
-            word_index.append(i) 
-        except ValueError:
-            return word_index
-        
-
-# This function is to retrieve the collocative words based on n - # words away from ambigious word
+# this function finds the index of the collocative word
 def find_coll(n, context):
-    coll_word_index = find_index(context, ambg_word)
-    coll_word_index = coll_word_index[0]
-    if len(context) > coll_word_index and coll_word_index >=0:
-        return context[coll_word_index] #indexes the context to get collocative word
+    n_word_index = context.index(ambg_word) + n    
+    if len(context) > n_word_index and n_word_index >= 0:
+        return context[n_word_index]
     else:
-        return "" #if n is out of bounds it will return a blank'  
-
+        return ""
 
 # This function adds the new conditions based on collocation to the decision list
 def write_cond(cfd, data, n):
@@ -92,24 +110,22 @@ def write_cond(cfd, data, n):
         sense, context = element['sense'], element['text']
         coll_word = find_coll(n, context)
         if coll_word != '':
-            condition = str(n) + "_word_" + coll_word
+            condition = str(n) + " Word: " + coll_word
             cfd[condition][sense] = cfd[condition][sense] + 1
     return cfd
-
 
 # To calculate the logarithm of likelihood for ratio of sense probabilities
 def log_likelihood(cpd, rule):
     s1_prob = cpd[rule].prob("phone")
     s2_prob = cpd[rule].prob("product")
     if (s1_prob/s2_prob) != 0:
-        return math.log((s1_prob/s2_prob),2)
+        return round(math.log((s1_prob/s2_prob),2),3)
     else:
         return 0
 
-
 # checking whether the rule is satisfied in a given context
 def check_rule(context, rule):
-    rule_scope, rule_type, rule_feature = rule.split("_")
+    rule_scope, rule_type, rule_feature = rule.split(" ")
     rule_scope = int(rule_scope)
     
     return find_coll(rule_scope, context) == rule_feature
@@ -157,9 +173,9 @@ def extract_test_data(file):
         extracted_data.append(sentence)
         
     return extracted_data
-    
-train_data = extract_training_data(training_data)
 
+#    
+train_data = extract_training_data(training_data)
 
 # Use conditional frequency distribution to add learned rules to the decision list
 cfd = ConditionalFreqDist()
@@ -169,8 +185,6 @@ for i in range (-3, 3):
         continue
     cfd = write_cond(cfd, train_data, i)
         
-        
-
 # Instantiating Condition probability distribution to calculate the probabilities of the frequencies recorded above
 cpd = ConditionalProbDist(cfd, LidstoneProbDist, 0.1)
 
@@ -181,33 +195,6 @@ for rule in cpd.conditions():
     decision_list.append([rule, likelihood, "phone" if likelihood > 0 else "product"])
     
     decision_list.sort(key=lambda rule: math.fabs(rule[1]), reverse=True)
-
-
-# extracting the test data through XML parsing
-#with open(testing_data, 'r') as data:
-#    test_soup = BeautifulSoup(data, 'html.parser')
-
-#test_data = []
-#for instance in test_soup('instance'):
-#    sntnc = dict()
-#    sntnc['id'] = instance['id']
-#    text = ""
-#    for s in instance.find_all('s'):
-#        text = text+ " "+ s.get_text()
-#    sntnc['text'] = process_text(text)
-#    test_data.append(sntnc)
-
-test_data = extract_test_data(testing_data)
-
-# Calculating the frequencies of each senses
-#senseA, senseB = 0.0, 0.0
-#for element in train_data:
- #   if element['sense'] == "phone":
-  #      senseA += 1.0
-   # elif element['sense'] == 'product':
-    #    senseB += 1.0
-    #else:
-     #   print("warning no match")
 
 #Searches the training data to count the frequency of each sense.
 #Method adapted from #https://www.kite.com/python/answers/how-to-search-if-dictionary-value-contains-certain-string-in-python
@@ -222,23 +209,15 @@ for i in range(0, len(train_data)):
     else:
         print('The word does not exist.')
 
-#Stores time it took for model to process input as 'stop time'    
-#stop_time = time.time()
-#subtract the two time periods to calculate the runtime of the decision list
-#prints the elapsed time in the my-line-answers file
-#print('Time elapsed:', round((stop_time - start_time),3), 'sec')
-
 #retreives percentage frequency of each sense.
 sensePercentage1 = round((sense1/textLen)*100,2)
 sensePercentage2 = round((sense2/textLen)*100,2)
 
-#print(sensePercentage1, '% of instances of the word line is a phone')
-#print(sensePercentage2, '% of instances of the word line is a product' + '\n')
-      
-
-
 # Calculating the majority sense
 majority_sense = "phone" if sense1 > sense2 else "product"
+
+# 
+test_data = extract_test_data(testing_data)
 
 # Performing the predictions
 predictions = []
@@ -255,4 +234,3 @@ for i in decision_list:         # loop through the decision list and write it to
     writer.write('%s\n' % i)
 
 writer.close()      # close the text file
-
--- decision_list/scorer.py
+++ decision_list/scorer.py
@@ -1,9 +1,33 @@
 '''
 AIT 590 - Assignment 3 
-
 Team 3 - Rafeef Baamer, Ashish Hingle, Rina Lidder, & Andy Nguyen
+Date: 3/31/2021
 
-This is the Scorer
+Description: This Python program scores the decision list created by decision_list.py. A confusion matric is generated based on the results of the 
+trained and tested data to be compared against a baseline.
+
+Libraries used: RE, SYS, PANDAS
+
+Usage Instructions: This program must be used in combination with the scorer.py program. To use the program:
+1) Ensure all supplmentary files are placed in the correct directory
+2) In the terminal, run the command "python scorer.pl my-line-answers.txt line-answers.txt"
+	a) scorer.py -> this file
+	b) my-line-answers.txt -> generated file from decision-list.py
+	c) line-answers.txt -> baseline file with ansers for each sense
+	
+2) The baseline accuracy, accuracy after adding features and confusion matrix are printed.
+ 
+Algorithm defined in program:
+
+
+Additional features:  
+
+Results of confusion Matrix:
+
+First 10 results of my-decision-list:
+
+First 10 results my-line-answers.txt:
+
 
 
 '''
@@ -18,8 +42,8 @@ import sys
 
 
 # command line arguments for the file sources of results obtained from decision-list and available gold standard
-my_key = sys.argv[1]
-gs_key = sys.argv[2]
+generated_file = sys.argv[1]
+baseline_file = sys.argv[2]
 
 #This function is to search each line from the outputs to create a dictionary of
 #the sysnets and word sense. NEW FUNCTION**
@@ -34,16 +58,6 @@ def all_word_senses(out_list):
         sense[key] = value
     return sense, keys
 
-'''def get_senses(mylist):
-    sense = {}
-    keys = []
-    for string in mylist:
-        search = re.search('<answer instance="(.*)" senseid="(.*)"/>', string, re.IGNORECASE)
-        key = search.group(1)
-        keys.append(key)
-        value = search.group(2)
-        sense[key] = value
-    return sense, keys'''
 #This function calculates the number of correct matches between the WSD model's output
 #and the predicted answers. It then uses the number of correct matches to calculate the accuracy.
 def accuracy_sense():
@@ -56,66 +70,36 @@ def accuracy_sense():
     return accuracy
 
 #open the two files taken as input from command line and strip out '\n'
-with open(gs_key, 'r') as data:
+with open(baseline_file, 'r') as data:
     mylist1 = [line.rstrip('\n') for line in data]
 current_sense, keys = all_word_senses(mylist1)
 
-with open(my_key, 'r') as data:
+with open(generated_file, 'r') as data:
     mylist2 = [line.rstrip('\n') for line in data]
 pred_sense, keys = all_word_senses(mylist2)
 
-
-
-'''
-# For a specific key check if the values matches
-correct = 0
-total = len(keys)
-for key in keys:
-    if(answers[key] == preds[key]):
-        correct += 1
-correct
-
-#caluclated the most frequent sense baseline
 total = len(keys)
 baseline_count = 0
 for key in keys:
     if(current_sense[key] == 'phone'):
         baseline_count = baseline_count + 1
 baseline_acc = round((baseline_count/total)*100,2)
-print("Baseline accuracy is "+str(baseline_acc)+"%") 
-
-
-# calucalate the accuracy after learning features
-#accuracy = (float(correct)/float(total))*100
-print("Accuracy after adding learned features is "+str(accuracy)+"%")'''
-
-
-total = len(keys)
-baseline_count = 0
-for key in keys:
-    if(current_sense[key] == 'phone'):
-        baseline_count = baseline_count + 1
-baseline_acc = round((baseline_count/total)*100,2)
-print("Baseline accuracy is "+str(baseline_acc)+"%") 
+print("Baseline accuracy: "+str(baseline_acc)+"%") 
 
 accuracy_output = print('Based on the collocative features extracted the accuracy is {}%'.format(accuracy_sense()))
 #creating array for our output and append the values to list
 
-'''
-pred_list = []
-for v in pred_sense:
-    pred_list.append(pred_sense[v])'''
+
 #Access the dictionary to retreive the predicted senses to a list.    
 
 #creating array for gold standard and append the values to its list'''
 
-answers_list = []
-for v in current_sense:
-    answers_list.append(current_sense[v])
+
 #access the dicitionary values for the current senses into a list 
 #https://realpython.com/python-dicts/#dvalues   
 pred_list = list(pred_sense.values())
 answer_list = list(current_sense.values())
+
 # creating dataframes for both the files
 df1 = pd.Series( (v for v in pred_list) )
 df2 = pd.Series( (v for v in answer_list) )
