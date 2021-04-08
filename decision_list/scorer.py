5e84927fff3ac97e30fb5a212fa62bd036c37cdd
Rina <rina635@gmail.com>
2021-04-07 21:20:14 -0400

    Added timer updated variables

--- decision_list/scorer.py
+++ decision_list/scorer.py
@@ -60,18 +60,19 @@ import pandas as pd
 import importlib
 import re
 import sys
-import string
-from bs4 import BeautifulSoup
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
+import string, time
 
-# command line arguments for the file sources of results obtained from decision-list and available gold standard
+
+#The command line arguments to run the scorer using the results obtained from decision-list.py (generated_file)
+#and the gold standard "key" data (key_file)
 generated_file = sys.argv[1]
-baseline_file = sys.argv[2]
-testing_data = sys.argv[3]
+key_file = sys.argv[2]
 
-#This function is to search each line from the outputs to create a dictionary of
-#the sysnets and word sense. NEW FUNCTION**
+#stores the current time of the request from user as the 'start time'
+start_time = time.time()
+
+#This function is to search each line from a file and create a dictionary of
+#the sysnets (key) and word sense (value) by spliting at the underscore character.
 def all_word_senses(out_list):
     sense = {}
     keys = []
@@ -83,120 +84,56 @@ def all_word_senses(out_list):
         sense[key] = value
     return sense, keys
 
-#This function calculates the number of correct matches between the WSD model's output
-#and the predicted answers. It then uses the number of correct matches to calculate the accuracy.
+#This function calculates the accuracy of the model using the number of correct matches between the WSD model's 
+#output (pred_senses) and the key data file (correct_sense)
 def accuracy_sense():
     correct = 0
     for key in keys:
-        if(current_sense[key]) == pred_sense[key]:
+        if(correct_sense[key]) == pred_sense[key]:
             correct = correct + 1
     accuracy = (correct/len(keys))*100
     accuracy = round(accuracy,2)
     return accuracy
-    
-# Copy of extracting test data function to determine most ambiguous context
-def extract_test_data(file):
-    with open(file, 'r') as data:
-        soup_data = BeautifulSoup(data, 'html.parser')
-    extracted_data = []
-    for instance in soup_data.find_all('instance'):
-        sentence = dict()
-        sentence['id'] = instance['id']
-        text = ""
-        for s in instance.find_all('s'):
-            text = text + " "+ s.get_text()
-        sentence['text'] = process_text(text)
-        extracted_data.append(sentence)
-        
-    return extracted_data
-    
-# Function to preprocess the text (lower case, remove stopword, stemming and remove html)
-def process_text(unprocessed_text):
-    # lower case all unprocessed text
-    unprocessed_text = unprocessed_text.lower()
-
-    # save stopwords into a variable for removal
-    sw_p = stopwords.words("english")
-    
-    # save punctuation symbols into the variable for removal 
-    sw_p.extend(string.punctuation)
-    
-    # stem and replace other forms of the root word in the text for consistency
-    ps = PorterStemmer()   # create a PorterStemmer class
-    other_forms = 'lines'   # indicate other forms of the root word   
-    unprocessed_text = unprocessed_text.replace(other_forms, ps.stem(other_forms))   # convert lines to line using the stemmed root word
-    
-    # remove sumbols that due to html tags
-    processed_text = [re.sub(r'[\.\,\?\!\'\"\-\_/]','',w) for w in unprocessed_text.split(" ")]
-    
-    # remove stopwords and punctuation 
-    processed_text = [w for w in processed_text if w not in sw_p and w != '']
-    
-    return processed_text   # return the processed text to the method that called it
 
 
-# open the two files taken as input from command line and strip out '\n'
-with open(baseline_file, 'r') as data:
+#Gathers the two files from command line, strips the '\n' and creates dictionaries
+
+#Key value for gold standard key data is: Key and Correct_sense
+with open(key_file, 'r') as data:
     mylist1 = [line.rstrip('\n') for line in data]
-current_sense, keys = all_word_senses(mylist1)
+correct_sense, keys = all_word_senses(mylist1)
 
+#Key value pair fore the generated file : Key and Pred_sense
 with open(generated_file, 'r') as data:
     mylist2 = [line.rstrip('\n') for line in data]
 pred_sense, keys = all_word_senses(mylist2)
 
+#Uses the total number of keys to calculate the baseline accuracy
 total = len(keys)
 baseline_count = 0
 for key in keys:
-    if(current_sense[key] == 'phone'):
+    if(correct_sense[key] == 'phone'):
         baseline_count = baseline_count + 1
 baseline_acc = round((baseline_count/total)*100,2)
 print("Baseline Accuracy: "+str(baseline_acc)+"%") 
 
-accuracy_output = print('Overall Accuracy: {}%'.format(accuracy_sense()))
-#creating array for our output and append the values to list
+#Prints the accuracy output in the command line.
+accuracy_output = print('Overall Accuracy: {}%\n'.format(accuracy_sense()))
 
 
-#Access the dictionary to retreive the predicted senses to a list.    
-
-#creating array for gold standard and append the values to its list'''
-
-
-#access the dicitionary values for the current senses into a list 
+#access the dicitionary values for the predicted sense and correct senses into a list 
 #https://realpython.com/python-dicts/#dvalues   
 pred_list = list(pred_sense.values())
-answer_list = list(current_sense.values())
+correct_list = list(correct_sense.values())
 
-# creating dataframes for both the files
-df1 = pd.Series( (v for v in pred_list) )
-df2 = pd.Series( (v for v in answer_list) )
+#Converts each of the lists into a dataframe for the confusion matrix.
+generated_file_df = pd.Series((v for v in pred_list))
+key_file_df = pd.Series((v for v in correct_list))
 
 #generating confusion matrix
-df_confusion = pd.crosstab(df1, df2)
-print("Confusion matrix is\n"  +str(df_confusion))
+df_confusion = pd.crosstab(generated_file_df, key_file_df)
+print("Confusion matrix:\n"  +str(df_confusion))
 
-# Extract testing data into list
-test_data = extract_test_data(testing_data)
-#print(test_data)
-
-#for element in test_data:
-#    id1 = element['id']
-#    text = element['text']
-
-incorrect_senses = []   
-for key in keys:
-    if(current_sense[key]) != pred_sense[key]:
-        print(len(current_sense.items()))
-        #key, value = current_sense.items()
-        #print(key)
-        #incorrect_senses.append(value)
-        
-        #print(current_sense.keys())
-        #for item in test_data:
-        #    print(item.get('text'))
-            #print(list(current_sense.keys())[0])
-        #print(test_data.get(current_sense.keys()))
-incorrect_senses = incorrect_senses[0]
-print(incorrect_senses)
-#for key in keys:
-#    if(current_sense[key]) != pred_sense[key]:
-#        print(current_sense[key])
+stop_time = time.time()
+#subtract the two time periods to calculate the runtime of the scorer:
+print('\nTime Executed :', (stop_time - start_time), 'seconds.')
